940a160612d7d6fbc9ac273232f82956eabfb8fc
Winterzune <winterzune@gmail.com>
2018-10-01 21:41:53 -0400

    k

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,30 @@ $(document).ready(function() {
 
 //counters
 
-var correctAnswers = 0
-var wrongAnswers = 0
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var qAndA = 0;
 
 
+questions = ["1) What house was Moaning Myrtle in?", "2) Who taught the History of Magic lessons?", "3) What is a Bezoar?", "4) What is Dumbledore's full name?", "5) Which of these is an unforgivable curse?", "6) What dragon breed does Harry face in the Goblet of Fire?", "7) Which of these is a horcrux that harry destroyed?", "8) What does the spell Accio do?", "9) What stops Fleur from finishing the second task?", "10) What's the name of Hermione's pet?"];
 
-var questions = ["1) What house was Moaning Myrtle in?", "2) Who taught the History of Magic lessons?", "3) What is a Bezoar?", "4) What is Dumbledore's full name?", "5) Which of these is an unforgivable curse?", "6) What dragon breed does Harry face in the Goblet of Fire?", "7) Which of these is a horcrux that harry destroyed?", "8) What does the spell Accio do?", "9) What stops Fleur from finishing the second task?", "10) What's the name of Hermione's pet?"]
+//if the radio input is cheked conditions
+//if the input chosen is equal to the values with the correct or wrong answers
 
-//var answers? 
 
-
-//when submit button is clicked, it should store the options chosen by the user
-$("#submits").on("click", function() {
-    var chosenOption = $('input[name=optionRadios]: checked').val();
-
+$('input[type=radio]').change(function(){
+    if ($('input.radio_check).attr(:'checked');
+    console.log(questions[0]);
 });
 
-//if the checked options have the class of .correctAnswer then:
+
+
+//$("#chosen").on("click", function() {
+  //  var chosenOption = $('input[name=optionRadios]: checked').val();
+//
+//});
+
+//if the checked options have the class of correctAnswer then:
 if (chosenOption = $(".correctAnswer")) {
     correctAnswers++;
     qAndA = qAndA - 1;
@@ -35,7 +42,8 @@ else {
     
 setTimeout (oneMinute, 60000);
 function oneMinute () {
-   alert("Times up!")
+   alert("Times up!");
+   $(".jumbotron").text('<p>setTimeout</p>');
 
 //Results Alert
 
